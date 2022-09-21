48302955897404fe6c862605101d9c646377e4a0
NoahMacRitchie <47670496+NoahMacRitchie@users.noreply.github.com>
2022-09-21 16:54:19 -0700

    Added initial readmes

--- src/typingpage.cpp
+++ src/typingpage.cpp
@@ -1,6 +1,5 @@
 // Local Includes
 #include "typingpage.h"
-
 #include "styledtextzone.h"
 #include "typingzone.h"
 #include "results.h"
@@ -11,11 +10,16 @@
 #include <QElapsedTimer>
 #include <QPlainTextEdit>
 #include <QLabel>
+#include <QPushButton>
 
 TypingPage::TypingPage(QWidget* parent) : QWidget(parent)
 {
     ResultsData currentData;
     auto typingScreenLay = new QVBoxLayout(this);
+    auto btnRowLay = new QHBoxLayout(this);
+    auto btnRowWid = new QWidget(this);
+
+    btnRowWid->setLayout(btnRowLay);
 
     auto mainContentLay = new QVBoxLayout(this);
 
@@ -27,6 +31,9 @@ TypingPage::TypingPage(QWidget* parent) : QWidget(parent)
     mainContentLay->addWidget(styledTextZone_);
     mainContentLay->addWidget(typingZone);
     mainContentWid->setLayout(mainContentLay);
+
+    auto resetBtn = new QPushButton("RESET", this);
+    auto newTextBtn = new QPushButton("NEW TEXT");
     timer_ = new Timer(this);
     auto score = new Results();
 
@@ -38,10 +45,16 @@ TypingPage::TypingPage(QWidget* parent) : QWidget(parent)
     QObject::connect(timer_, &Timer::timeStopped, this, &TypingPage::storeData);
     QObject::connect(typingZone, &TypingZone::textChanged, timer_, &Timer::startTimer);
 
-    typingScreenLay->addWidget(timer_);
+    resetBtn->setMaximumWidth(100);
+    newTextBtn->setMaximumWidth(100);
+
+    btnRowLay->addWidget(timer_);
+    btnRowLay->addWidget(resetBtn);
+    btnRowLay->addWidget(newTextBtn);
+    typingScreenLay->addWidget(btnRowWid);
     typingScreenLay->addWidget(mainContentWid);
 
-    QString text("while(true){\n    useCodeSpeed()\n}");
+    QString text("while(true){\n    useCodeSpeed();\n}");
     styledTextZone_->setNewText(text);
    
     setLayout(typingScreenLay);
@@ -49,7 +62,7 @@ TypingPage::TypingPage(QWidget* parent) : QWidget(parent)
 
 void TypingPage::reset()
 {
-    QString text("while(true){\n    useCodeSpeed()\n}");
+    QString text("while(true){\n    useCodeSpeed();\n}");
     styledTextZone_->setNewText(text);
 
     timer_->reset();
